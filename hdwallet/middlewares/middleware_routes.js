6fbcd1d55a76f2e8e78714f37854cb1342fd14bd
Kahan Majmudar <kahan.m.btechi16@ahduni.edu.in>
2020-06-03 14:21:07 +0530

    restructured

--- hdwallet/middlewares/middleware_routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { AuthController } from './middleware'
-const router = Router()
-const auth = new AuthController()
-
-router.post('/login', auth.login)
-
-module.exports = router
